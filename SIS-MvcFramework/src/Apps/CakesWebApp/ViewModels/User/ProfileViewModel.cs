128b0f58432a37b849b579b4fd6dd23a13240bf0
Yordan <pm.yordan@gmail.com>
2018-10-23 11:50:21 +0300

    Added profile page.

---
+++ SIS-MvcFramework/src/Apps/CakesWebApp/ViewModels/User/ProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CakesWebApp.ViewModels.User
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ProfileViewModel
+    {
+        public string Username { get; set; }
+
+        public DateTime DateOfRegister { get; set; }
+
+        public int OrdersCount { get; set; }
+    }
+}
