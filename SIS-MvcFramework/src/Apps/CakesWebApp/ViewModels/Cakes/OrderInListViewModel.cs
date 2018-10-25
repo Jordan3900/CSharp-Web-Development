5ac91dbe1479e2b5e1d3ed6a2d1954a7f70bb633
Yordan <pm.yordan@gmail.com>
2018-10-25 19:59:12 +0300

    Commit automapping funcitionality.

---
+++ SIS-MvcFramework/src/Apps/CakesWebApp/ViewModels/Cakes/OrderInListViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace CakesWebApp.ViewModels.Cakes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class OrderInListViewModel
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int NumberOfProducts { get; set; }
+
+        public decimal SumOfProducts { get; set; }
+    }
+}
---
+++ SIS-MvcFramework/src/Apps/CakesWebApp/ViewModels/Orders/GetByIdViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace CakesWebApp.ViewModels.Orders
+{
+    using CakesWebApp.ViewModels.Cakes;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class GetByIdViewModel
+    {
+        public int Id { get; set; }
+
+        public bool IsShoppingCart { get; set; }
+
+        public IEnumerable<CakeViewModel> Products { get; set; }
+    }
+}
