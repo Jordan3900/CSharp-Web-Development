ec0e400ca307c48b7496eb16994a24e2719cd94a
Yordan <pm.yordan@gmail.com>
2018-10-26 15:21:59 +0300

    Add mish-mash exam.

---
+++ SIS-MvcFramework/src/Mish-Mash-Exam/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿namespace Mish_Mash_Exam.Controllers
+{
+    using SIS.MvcFramework;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public abstract class BaseController : Controller
+    {
+    }
+}
---
+++ SIS-MvcFramework/src/Mish-Mash-Exam/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace MishMashWebApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using SIS.MvcFramework;
+    using SIS.MvcFramework.Logger;
+    using SIS.MvcFramework.Services;
+
+
+    class Startup : IMvcApplication
+    {
+        public void Configure()
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection collection)
+        {
+            collection.AddService<ILogger, ConsoleLogger>();
+        }
+    }
+}
